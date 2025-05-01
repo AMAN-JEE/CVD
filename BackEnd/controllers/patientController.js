55a280555cddcd55fd60e3618f2e368b71839afc
KUMAR-AMAN <kumaraman15856@gmail.com>
2025-05-01 22:51:35 +0530

    fixed inegration of blood presure in DB

--- BackEnd/controllers/patientController.js
+++ BackEnd/controllers/patientController.js
@@ -1,25 +1,38 @@
-import Patient from '../models/patientModel.js';
+import Patient from "../models/patientModel.js";
 
 // Controller function to store patient details (Stage-1)
 const storePatientDetails = async (req, res) => {
-    const { name, age, gender, chestPain, serumCholesterol, exerciseAngina, majorVessels, thalliumTest } = req.body;
-    
-    try {
-        const patient = new Patient({
-            name,
-            age,
-            gender,
-            chestPain,
-            serumCholesterol,
-            exerciseAngina,
-            majorVessels,
-            thalliumTest,
-        });
-        await patient.save();
-        res.status(200).json({ message: 'Patient details saved successfully.' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error saving patient details.' });
-    }
+  const {
+    name,
+    age,
+    gender,
+    chestPain,
+    serumCholesterol,
+    exerciseAngina,
+    majorVessels,
+    thalliumTest,
+    systolicPressure,
+    diastolicPressure,
+  } = req.body;
+
+  try {
+    const patient = new Patient({
+      name,
+      age,
+      gender,
+      chestPain,
+      serumCholesterol,
+      exerciseAngina,
+      majorVessels,
+      thalliumTest,
+      systolicPressure,
+      diastolicPressure,
+    });
+    await patient.save();
+    res.status(200).json({ message: "Patient details saved successfully." });
+  } catch (error) {
+    res.status(500).json({ error: "Error saving patient details." });
+  }
 };
 
 export default { storePatientDetails };
--- BackEnd/models/patientModel.js
+++ BackEnd/models/patientModel.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 
 const patientSchema = new mongoose.Schema({
-    name: { type: String, required: true},
-    age: { type: Number, required: true },
-    gender: { type: Number, required: true },
-    chestPain: { type: Number, required: true },
-    serumCholesterol: { type: Number, required: true },
-    exerciseAngina: { type: Number, required: true },
-    majorVessels: { type: Number, required: true },
-    thalliumTest: { type: Number, required: true },
-    heartRate: { type: Number },  // Added new field
-    bloodPressure: { type: Number },  // Added new field
-    restingElectro: { type: [Number] },  // Added new field
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: Number, required: true },
+  chestPain: { type: Number, required: true },
+  serumCholesterol: { type: Number, required: true },
+  exerciseAngina: { type: Number, required: true },
+  majorVessels: { type: Number, required: true },
+  thalliumTest: { type: Number, required: true },
+  heartRate: { type: Number }, // Optional field
+  systolicPressure: { type: Number, required: true },
+  diastolicPressure: { type: Number, required: true },
 }, { timestamps: true });
 
 export default mongoose.model('Patient', patientSchema);
