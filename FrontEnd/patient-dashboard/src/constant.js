06fcbcc6459ec4c04adf50e845b002b448964379
= <kumaraman15856@gmail.com>
2025-06-08 16:42:28 +0530

    change the sensor server url

--- FrontEnd/patient-dashboard/src/constant.js
+++ FrontEnd/patient-dashboard/src/constant.js
@@ -1,4 +1,5 @@
 const baseurl = "https://cvd-zsxb.onrender.com";
-const baseurlsensor = "https://sensorbackend.onrender.com";
+// const baseurlsensor = "https://sensorbackend.onrender.com";
+const baseurlsensor = "https://sensorbackend-production.up.railway.app";
 
 export default {baseurl, baseurlsensor};
